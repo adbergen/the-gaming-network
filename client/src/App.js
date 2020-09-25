663d458cefa9758f464483ee9438131fcef22567
Anthony Bergen <anthonydbergen@gmail.com>
2020-09-25 11:17:06 -0400

    added auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import Books from "./pages/Games";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import NavBar from "./components/Nav/nav-bar";
+import Loading from "./components/Loading/loading";
 
-function App() {
+const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <Router>
       <div>
-        <Nav />
+        <NavBar />
         <Switch>
           <Route exact path={["/", "/games"]}>
             <Books />
@@ -24,6 +32,6 @@ function App() {
       </div>
     </Router>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/Nav/nav-bar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { NavLink as RouterNavLink } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "../logout-button/logout-button";
+import LoginButton from "../login-button/login-button";
+
+const MainNav = () => (
+  <Nav className="mr-auto">
+    <Nav.Link
+      as={RouterNavLink}
+      to="/"
+      exact
+      activeClassName="router-link-exact-active"
+    >
+      Home
+    </Nav.Link>
+    <Nav.Link
+      as={RouterNavLink}
+      to="/profile"
+      exact
+      activeClassName="router-link-exact-active"
+    >
+      Profile
+    </Nav.Link>
+    <Nav.Link
+      as={RouterNavLink}
+      to="/chat"
+      exact
+      activeClassName="router-link-exact-active"
+    >
+      Chat
+    </Nav.Link>
+  </Nav>
+);
+
+const AuthNav = () => {
+  const { isAuthenticated } = useAuth0();
+  console.log("AuthNav isAuthenticated", isAuthenticated);
+
+  return (
+    <Nav className="justify-content-end">
+      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+    </Nav>
+  );
+};
+
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="md">
+      <Container>
+        <Navbar.Brand as={RouterNavLink} className="logo" to="/" />
+        <MainNav />
+        <AuthNav />
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
