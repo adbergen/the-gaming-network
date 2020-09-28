d6079df994701c17eafce2df277c38d47f734186
Anthony Bergen <anthonydbergen@gmail.com>
2020-09-28 12:14:12 -0400

    heroku deploy

---
+++ client/build/precache-manifest.da0c01093ab21047ebf45ee51c708ae0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff1722bd08886411f9567496f0140c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f133b1b2873a781ec8d",
+    "url": "/static/css/main.b7ae4841.chunk.css"
+  },
+  {
+    "revision": "0037f0cb4cf0b6fce848",
+    "url": "/static/js/2.a85e3223.chunk.js"
+  },
+  {
+    "revision": "7f0fa9728c0b97318c894f77c66a3cf5",
+    "url": "/static/js/2.a85e3223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f133b1b2873a781ec8d",
+    "url": "/static/js/main.892ac1fa.chunk.js"
+  },
+  {
+    "revision": "53d69f4a0c140d0ca5b5",
+    "url": "/static/js/runtime-main.f2447cd3.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "cf98a1194fe66ee474c800d153e9850a",
+    "url": "/static/media/logo.cf98a119.webp"
+  },
+  {
+    "revision": "4b1bb228bf826befdb5e9d530b8738fa",
+    "url": "/static/media/profile.4b1bb228.png"
+  }
+]);
