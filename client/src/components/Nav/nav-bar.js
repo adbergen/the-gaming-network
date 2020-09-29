3c3ff468156dab8ceffbfbdd932c7e139da07148
hojin Mun <jin1281@gmail.com>
2020-09-29 16:06:19 -0400

    protected routes

--- client/src/components/Nav/nav-bar.js
+++ client/src/components/Nav/nav-bar.js
@@ -10,53 +10,62 @@ import logo from "../../assets/logo.webp";
 import Image from "react-bootstrap/Image";
 
 import Profile from "../Profile/profile";
-const MainNav = () => (
-  
-  
-  
-  <Nav className="mr-auto">
-    <Image
-      className="logo"
-      src={logo}
-      roundedCircle
-      style={{ height: "auto", width: "20%" }}
-    />
-    <Nav.Link
-      as={RouterNavLink}
-      to="/"
-      exact
-      activeClassName="router-link-exact-active"
-      style={{ color: "white" }}
-    > 
-      Home
-    </Nav.Link>
-    <Nav.Link
-      as={RouterNavLink}
-      to="/dashboard"
-      exact
-      activeClassName="router-link-exact-active"
-      style={{ color: "white" }}
-    >
-      Dashboard
-    </Nav.Link>
-    <NavDropdown 
-    title="Chat" 
-    id="collasible-nav-dropdown"
-    style={{ color: "white" }}>
-        <NavDropdown.Item 
-        as={RouterNavLink}
-    to="/chat"
-    exact
-    activeClassName="router-link-exact-active"
-        href="/chat">Global</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.2">Battle Royal</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">MMO</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.4">Single Player</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.4">Call of Duty</NavDropdown.Item>
+const MainNav = () => {
+  const { isAuthenticated } = useAuth0();
 
-      </NavDropdown>
-    {/* <Nav.Link
+  return (
+    <Nav className="mr-auto">
+      <Image
+        className="logo"
+        src={logo}
+        roundedCircle
+        style={{ height: "auto", width: "20%" }}
+      />
+      {isAuthenticated && (
+        <Nav.Link
+          as={RouterNavLink}
+          to="/"
+          exact
+          activeClassName="router-link-exact-active"
+          style={{ color: "white" }}
+        >
+          Home
+        </Nav.Link>
+      )}
+      {isAuthenticated && (
+        <Nav.Link
+          as={RouterNavLink}
+          to="/dashboard"
+          exact
+          activeClassName="router-link-exact-active"
+          style={{ color: "white" }}
+        >
+          Dashboard
+        </Nav.Link>
+      )}
+      {isAuthenticated && (
+        <NavDropdown
+          title="Chat"
+          id="collasible-nav-dropdown"
+          style={{ color: "white" }}
+        >
+          <NavDropdown.Item
+            as={RouterNavLink}
+            to="/chat"
+            exact
+            activeClassName="router-link-exact-active"
+            href="/chat"
+          >
+            Global
+          </NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.2">Battle Royal</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">MMO</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4">Single Player</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4">Call of Duty</NavDropdown.Item>
+        </NavDropdown>
+      )}
+      {/* <Nav.Link
       as={RouterNavLink}
       to="/chat"
       exact
@@ -65,8 +74,9 @@ const MainNav = () => (
     >
       Chat
     </Nav.Link> */}
-  </Nav>
-);
+    </Nav>
+  );
+};
 
 const AuthNav = () => {
   const { isAuthenticated } = useAuth0();
