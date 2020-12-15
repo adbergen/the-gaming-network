a91ff7c1e2dfc62996c635684e2572bbb72ec14b
Anthony Bergen <anthonydbergen@gmail.com>
2020-12-15 14:49:11 -0500

    grouped slide files together

--- client/src/components/Slides/containers/page_2/page_2.js
+++ client/src/components/Slides/containers/page_2/page_2.js
@@ -2,11 +2,11 @@ import React from "react";
 
 import styles from "./page_2.module.css";
 
-import pic4 from "../../img/pic4.png";
-import First from "../../components/Slides/FirstSlide";
-import Last from "../../components/last/last";
-import Mid from "../../components/mid/mid";
-import PageTwo from "../../components/page_2/page_2";
+import pic4 from "../../../../assets/pic4.png";
+import First from "../../FirstSlide";
+import Last from "../../last/last";
+import Mid from "../../mid/mid";
+import PageTwo from "../../page_2/page_2";
 
 class Page2 extends React.Component {
   render() {
